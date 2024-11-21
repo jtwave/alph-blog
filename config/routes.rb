0deb48cb6c17255366a512cb288c1c49dafac6bd
John Taylor <johntaylor914@outlook.com>
2024-11-20 19:18:49 -0500

    added ability to update articles

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :articles, only: [:show, :index]
+  resources :articles, only: [:show, :index, :new,  :create, :edit, :update]
   
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
